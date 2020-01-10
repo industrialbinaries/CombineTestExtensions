0b68767b438581dd57b59146844c2bb38502aa8e
Vojta Stavik <stavik@outlook.com>
2020-01-10 16:41:19 +0100

    Finish the first version of TestScheduler implementation

--- Sources/CombineTestExtensions/TestScheduler.swift
+++ Sources/CombineTestExtensions/TestScheduler.swift
@@ -1,33 +1,38 @@
+//
+//  CombineTestExtensions
+//
+//  Copyright (c) 2020 Industrial Binaries
+//  MIT license, see LICENSE file for details
+//
+
 import Combine
 import Foundation
 
+/// A `Scheduler` implementation for using in tests. It uses `Int` for its
+/// time type.
+///
+/// The scheduler is inactive until `resume()` is called.
+///
 public class TestScheduler: Scheduler {
-
-  public typealias SchedulerTimeType = Int
   public typealias SchedulerOptions = Never
 
-  public func resume() {
-    var tasks = self.tasks.sorted { $0.time < $1.time }
+  public var now: Int = 0
+  public var minimumTolerance: Int = 0
 
+  /// Starts the scheduler. All scheduled actions are performed immediately and synchronously.
+  public func resume() {
     while !tasks.isEmpty {
-      autoreleasepool {
-        let task = tasks.removeFirst()
-        currentTime = task.time
-        task.action()
-      }
+      // This sort is stable https://forums.swift.org/t/is-sort-stable-in-swift-5/21297
+      tasks.sort { $0.time < $1.time }
+
+      let task = tasks.removeFirst()
+      now = task.time
+      task.action()
     }
   }
 
-  private var tasks: [Task] = []
-
-  var currentTime = 0
-
-  public var now: Int { currentTime }
-
-  public var minimumTolerance: Int { 1 }
-
   public func schedule(options: Never?, _ action: @escaping () -> Void) {
-    schedule(after: currentTime, tolerance: 0, options: options, action)
+    schedule(after: now, tolerance: 0, options: options, action)
   }
 
   public func schedule(after date: Int, tolerance: Int, options: Never?, _ action: @escaping () -> Void) {
@@ -44,42 +49,48 @@ public class TestScheduler: Scheduler {
   ) -> Cancellable {
     fatalError("Not implemented yet.")
   }
+
+  /// Creates a new TestScheduler.
+  public init() {}
+
+  private var tasks: [Task] = []
 }
 
 extension TestScheduler {
-
-  struct Task: Hashable {
-    let time: SchedulerTimeType
+  fileprivate struct Task: Hashable {
+    let time: Int
     let action: () -> Void
 
     private let id: UUID = .init()
+  }
+}
 
-    static func == (lhs: TestScheduler.Task, rhs: TestScheduler.Task) -> Bool { lhs.id == rhs.id }
+extension TestScheduler.Task {
+  static func == (lhs: TestScheduler.Task, rhs: TestScheduler.Task) -> Bool { lhs.id == rhs.id }
 
-    func hash(into hasher: inout Hasher) {
-      hasher.combine(id)
-    }
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(id)
   }
 }
 
 extension Int: SchedulerTimeIntervalConvertible {
   public static func seconds(_ s: Int) -> Int {
-      return .init(s)
+    return .init(s)
   }
 
   public static func seconds(_ s: Double) -> Int {
-      return .init(Int(s + 0.5))
+    fatalError("Not supported yet.")
   }
 
   public static func milliseconds(_ ms: Int) -> Int {
-      .seconds(Double(ms) / 1e+3)
+    fatalError("Not supported yet.")
   }
 
   public static func microseconds(_ us: Int) -> Int {
-      .seconds(Double(us) / 1e+6)
+    fatalError("Not supported yet.")
   }
 
   public static func nanoseconds(_ ns: Int) -> Int {
-      .seconds(Double(ns) / 1e+9)
+    fatalError("Not supported yet.")
   }
 }
---
+++ Tests/CombineTestExtensionsTests/TestSchedulerTests.swift
@@ -0,0 +1,31 @@
+//
+//  CombineTestExtensions
+//
+//  Copyright (c) 2020 Industrial Binaries
+//  MIT license, see LICENSE file for details
+//
+
+@testable import CombineTestExtensions
+
+import XCTest
+
+final class TestSchedulerTests: XCTestCase {
+  func testSchedullingWorksCorrectly() {
+    let scheduler = TestScheduler()
+
+    var result: [Int] = []
+
+    scheduler.schedule(after: 100) { result.append(4) }
+    scheduler.schedule(after: 70) { result.append(3) }
+
+    scheduler.schedule(after: 50) {
+      scheduler.schedule(after: 60) { result.append(2) }
+    }
+
+    scheduler.schedule { result.append(1) }
+
+    scheduler.resume()
+
+    XCTAssertEqual(result, [1, 2, 3, 4])
+  }
+}
